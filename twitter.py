e26d2d182fb9f3d52ae7e59c3543ac42a39eff1b
Miguel Martinez <miguelmalvarez@gmail.com>
2019-10-15 08:58:32 +0100

    Metereology factors included and better message formatting

--- twitter.py
+++ twitter.py
@@ -14,11 +14,14 @@ def authorise_twitter_api(config):
 def tweet_recommendation(config, weather_forecast, recommendation):
     auth = authorise_twitter_api(config)
     api = API(auth, wait_on_rate_limit=True)
-    message = 'Weather for {} in London\n'.format(weather_forecast['morning']['dt'].strftime("%A %d"))
-    message += 'Morning - {} degrees (C) with {} kph wind. '.format(weather_forecast['morning']['min_temperature'],
-                                                                    weather_forecast['morning']['wind'])
-    message += 'Afternoon - {} degrees with {} kph wind.\n'.format(weather_forecast['afternoon']['min_temperature'],
-                                                                   weather_forecast['afternoon']['wind'])
+    message = 'Weather for {} in London'.format(weather_forecast['morning']['dt'].strftime("%A %d"))
+    message += '\nMorning: {} | {} °C. {} kph wind. '.format(weather_forecast['morning']['meteorology'],
+                                                             weather_forecast['morning']['min_temperature'],
+                                                             weather_forecast['morning']['wind'])
+    message += '\nAfternoon: {} | {} °C. {} kph wind.\n'.format(weather_forecast['afternoon']['meteorology'],
+                                                                weather_forecast['afternoon']['min_temperature'],
+                                                                weather_forecast['afternoon']['wind'])
 
     message += recommendation
+    print(message)
     return api.update_status(message)
