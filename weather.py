ad39b7c5abfb5bd7c064b17f471194350982a968
Miguel Martinez <miguelmalvarez@gmail.com>
2019-10-16 09:27:55 +0100

    meteorogoly codes used

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime, timedelta
 
-## TODO: Find rain probability
+# TODO: Find rain probability
 
 
 def time_slot_datapoint(openweather_response, day_to_predict, starting_timings):
@@ -17,17 +17,18 @@ def time_datapoint(openweather_response, day_to_predict, time):
 
 
 def extract_timeslot_weather_data(datapoint):
-    weather = {}
-    weather['dt'] = datetime.fromtimestamp(datapoint['dt'])
-    weather['min_temperature'] = datapoint['main']['temp_min']
-    weather['max_temperature'] = datapoint['main']['temp_max']
-    weather['humidity'] = datapoint['main']['humidity']
-    weather['wind'] = datapoint['wind']['speed']
-    weather['meteorology'] = datapoint["weather"][0]['description']
-    return weather
+    return {'dt': datetime.fromtimestamp(datapoint['dt']),
+            'min_temperature': datapoint['main']['temp_min'],
+            'max_temperature': datapoint['main']['temp_max'],
+            'humidity': datapoint['main']['humidity'],
+            'wind': datapoint['wind']['speed'],
+            'meteorology': datapoint["weather"][0]['description'],
+            # TODO: Look to use "Main" rather than id
+            'meteorology_code': datapoint["weather"][0]['id']}
 
 
 # It returns weather forecast data for today (if ran before 7am) or for tomorrow otherwise
+# List of weather conditions: https://openweathermap.org/weather-conditions
 def weather_data(config, morning_time=7, afternoon_time=18):
     weather_api_key = config['DEFAULT']['open_weather_key']
 
@@ -36,9 +37,8 @@ def weather_data(config, morning_time=7, afternoon_time=18):
     city = 'London,uk'
     request = base_url + '?q={}&appid={}&units=metric'.format(city, weather_api_key)
     data = requests.get(request).json()
-    day_weather = {}
-    day_weather['sunrise'] = datetime.fromtimestamp(data['sys']['sunrise'])
-    day_weather['sunset'] = datetime.fromtimestamp(data['sys']['sunset'])
+    day_weather = {'sunrise': datetime.fromtimestamp(data['sys']['sunrise']),
+                   'sunset': datetime.fromtimestamp(data['sys']['sunset'])}
 
     # Forecast 3-hour windows data
     base_url = 'http://api.openweathermap.org/data/2.5/forecast'
@@ -53,7 +53,7 @@ def weather_data(config, morning_time=7, afternoon_time=18):
     else:
         day_to_predict = now.date()
 
-    weather = {'morning' : {'sunrise': day_weather['sunrise']},
+    weather = {'morning': {'sunrise': day_weather['sunrise']},
                'afternoon': {'sunset': day_weather['sunset']}}
 
     weather['morning'].update(extract_timeslot_weather_data(time_datapoint(data, day_to_predict, morning_time)))
